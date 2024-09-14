39bb653ab69e318930b41bd8e0d3ffc29c5f9bd9
Cristopher Salazar <salazar6cristopher@gmail.com>
2024-09-14 18:46:23 -0500

    Update Profesor.java

--- src/main/java/utp/edu/pe/poo/model/Profesor.java
+++ src/main/java/utp/edu/pe/poo/model/Profesor.java
@@ -1,1 +1,8 @@
+package utp.edu.pe.poo.model;
 
+public class Profesor extends Usuario {
+    public Profesor(String nombres, String apellidos, String dni) {
+        super(nombres, apellidos, dni);
+        this.tipo = "Profesor";
+    }
+}
